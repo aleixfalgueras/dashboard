cd9d4f6280d199e8fcacce5b27dc2667da40efb1
Aleix Falgueras Casals <feina.aleix@gmail.com>
2025-07-30 13:19:34 +0300

    fix getServerSession

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth'
 import { UserRole } from '@prisma/client'
+import { authOptions } from '@/lib/auth'
 
 export default async function Home() {
-  const session = await getServerSession()
+  const session = await getServerSession(authOptions)
   
   if (!session) {
     redirect('/login')
